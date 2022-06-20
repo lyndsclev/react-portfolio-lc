9af6233d064c8e3fe87905455cc191d6fdc0a479
lyndsclev <lyndsclev@gmail.com>
2022-06-20 15:27:59 -0400

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import li from '../assets/icons/li-icon.png'; 
 import gh from '../assets/icons/github-icon.png'; 
-import so from '../assets/icons/stack-overflow-icon.png'; 
 
 const Footer = () => {
     return (
@@ -13,11 +12,6 @@ const Footer = () => {
             <a href='https://github.com/lyndsclev' target="_blank" rel="noreferrer">
                 <span className='icon is-large m-2'>
                     <img src={gh} alt='github' />
-                </span>
-            </a>
-            <a href='https://stackoverflow.com/users/17627901/lyndsey-clevesy' target="_blank" rel="noreferrer">
-                <span className='icon is-large m-2'>
-                    <img src={so} alt='stack overflow' />
                 </span>
             </a>
         </div>
