21c56b7ec7e54ebc8ed07a1b4278237d6b9693c2
lyndsclev <lyndsclev@gmail.com>
2022-06-21 14:56:41 -0400

    form works

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,32 @@
 import avatar from '../assets/avatar-profile-picture.png';
+import Quote from './Quote'; 
 
 const About = () => {
     return (
-        <div className='columns'>
-            <div className='column is-3'>
-                <figure className='image is-1x1'>
-                    <img className='is-rounded' src={avatar} alt='' />
-                </figure>
-            </div>
-            <div className='column'>
-                <div className='content'>
-                    <h2>ABOUT</h2>
-                    <p>LC is a digital marketing professional turned coder living in the great town of historic downtown Wilmington, NC. She loves puppers and all things related to donuts... Well, really just eating them.</p>
-                    <p>Her front-end development skillset includes: HTML, CSS, JavaScript, React, Node, Express, Bulma, Bootstrap, and the list goes on.</p>
+        <div>
+            <div className='columns'>
+                <div className='column is-3'>
+                    <figure className='image is-1x1'>
+                        <img className='is-rounded' src={avatar} alt='' />
+                    </figure>
+                </div>
+                <div className='column'>
+                    <div className='content'>
+                        <h2>ABOUT</h2>
+                        <p>With 13+ years of both agency and client-side marketing experience, I’m excited to have recently added front-end development to my skillset.</p> 
+                        <p>I’m passionate about user experience-driven design and love leveraging custom CSS along with CSS frameworks such as Bootstrap and Bulma. Diving in headfirst to (and ultimately mastering!) a new CSS framework is one of my favorite challenges!</p>
+                        <p>I’m strongly experienced in HTML5, CSS3, JavaScript, Express.js, React.js, Node.js, Agile methodology, and Git.</p>
+                        <p>I’ve also worked with jQuery, MongoDB, MySQL, and more.</p>
+                        <p>From a marketing perspective, I bring a strong project management background to the table and am an expert in automation. I believe in a data-driven approach and am strongly skilled in using both Marketo and HubSpot along with fully integrated CRM systems, including Salesforce.</p>
+                    </div>
                 </div>
             </div>
+            <div className="section">
+                <Quote />
+            </div>
         </div>
+        
+
     ); 
 }
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,46 +1,80 @@
+import { useState } from 'react'; 
+import emailjs from 'emailjs-com'; 
+
 const Contact = () => {
+
+    const [name, setName] = useState(''); 
+    const [email, setEmail] = useState(''); 
+    const [message, setMessage] = useState(''); 
+    const [emailSent, setEmailSent] = useState(false); 
+
+    const submit = () => {
+        if (name && email && message) {
+        
+            const serviceId = 'service_0wkrqjr'; 
+            const templateId = 'template_fj5f7c6'; 
+            const userId = 'lepU3VsqhPm2KaKjR'; 
+            const templateParams = {
+                name, 
+                email, 
+                message
+            }; 
+
+            emailjs.send(serviceId, templateId, templateParams, userId)
+                .then(response => console.log(response))
+                .then(error => console.log(error)); 
+
+            setName(''); 
+            setEmail(''); 
+            setMessage(''); 
+            setEmailSent(true); 
+
+            alert('Thank you for your message! I will be in touch soon.'); 
+            
+        } else {
+            alert('Please fill in all fields.'); 
+        }
+    }
+
     return (
         <div>
             <div className='content'>
                 <h2>CONTACT</h2>
             </div>
-            <div className='field is-horizontal'>
-                <div className='field-body'>
-                    <div className='field'>
-                        <p className='control is-expanded'>
-                            <input className='input' type='text' placeholder='Name' />
-                        </p>
-                    </div>
-                    <div className='field'>
-                        <p className='control is-expanded'>
-                            <input className='input' type='text' placeholder='Email' />
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div className='field is-horizontal'>
-                <div className='field-body'>
-                    <div className='field'>
-                        <div className='control'>
-                            <input className='input' type='text' placeholder='Subject' />
+            <div id="contact-form">
+                <div className='field is-horizontal'>
+                    <div className='field-body'>
+                        <div className='field'>
+                            <div className='control'>
+                                <input className='input' type='text' placeholder='Name' value={name} onChange={e => setName(e.target.value)} />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className='field is-horizontal'>
-                <div className='field-body'>
-                    <div className='field'>
-                        <div className='control'>
-                            <textarea className='textarea' placeholder='Message'></textarea>
+                  <div className='field is-horizontal'>
+                    <div className='field-body'>
+                        <div className='field'>
+                            <div className='control'>
+                                <input className='input' type='email' placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className='field is-horizontal'>
-                <div className='field-body'>
-                    <div className='field'>
-                        <div className='control'>
-                            <button className='button is-link'>SEND MESSAGE</button>
+                <div className='field is-horizontal'>
+                    <div className='field-body'>
+                        <div className='field'>
+                            <div className='control'>
+                                <textarea className='textarea' placeholder='Message' value={message} onChange={e => setMessage(e.target.value)}></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='field is-horizontal'>
+                    <div className='field-body'>
+                        <div className='field'>
+                            <div className='control'>
+                                <button onClick={submit} className='button is-link'>SEND MESSAGE</button>
+                            </div>
                         </div>
                     </div>
                 </div>
