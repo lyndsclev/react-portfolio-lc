747c242765bfd67e9a4fbd823323d59748b51399
lyndsclev <lyndsclev@gmail.com>
2022-06-23 15:52:20 -0400

    updated quote

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -2,14 +2,12 @@ const Quote = () => {
     return (
         <div className="box">
             <div className="content">
-                <blockquote>
-                    <p><span id="quote-mark">"</span>I had the pleasure of managing Lyndsey for over a year. During that time, she showed an extreme willingness to learn, never backed down from a challenge, and ALWAYS asked questions when she didn't know the answer.</p>
-                    <p>When she was first hired, Marketo was a very new product for her and she dove headfirst into learning the product. Through watching videos, attending training, joining the user group, etc... she was able to learn a new system and be comfortable using it in a very short amount of time.</p> 
-                    <p>After learning the system, Lyndsey was always looking for ways to improve it and dealing with the hiccups along the way. Many times we would fix one thing just to discover 3 more that were hidden beneath. All throughout, she maintained a can-do attitude and never backed down.</p>
-                    <p>Finally, she was a consummate learner. Always asking questions, reading articles, sharing insights with the team, and staying up-to-date on latest trends. It was a pleasure having her on the team!</p>
-                    <p>I can't recommend Lyndsey enough for any role. She has shown that even if she might lack experience with a certain product, her attitude and willingness to learn is an asset for any organization!<span id="quote-mark">"</span></p>
-                    <p className="is-italic"> - <span className="has-text-weight-bold">Brett Carpenter</span>, Marketing Ops | Automation | MarTech Seamster</p>
-                </blockquote>
+                <p><span id="quote-mark">"</span>I had the pleasure of managing Lyndsey for over a year. During that time, she showed an extreme willingness to learn, never backed down from a challenge, and ALWAYS asked questions when she didn't know the answer.</p>
+                <p>When she was first hired, Marketo was a very new product for her and she dove headfirst into learning the product. Through watching videos, attending training, joining the user group, etc... she was able to learn a new system and be comfortable using it in a very short amount of time.</p> 
+                <p>After learning the system, Lyndsey was always looking for ways to improve it and dealing with the hiccups along the way. Many times we would fix one thing just to discover 3 more that were hidden beneath. All throughout, she maintained a can-do attitude and never backed down.</p>
+                <p>Finally, she was a consummate learner. Always asking questions, reading articles, sharing insights with the team, and staying up-to-date on latest trends. It was a pleasure having her on the team!</p>
+                <p>I can't recommend Lyndsey enough for any role. She has shown that even if she might lack experience with a certain product, her attitude and willingness to learn is an asset for any organization!<span id="quote-mark">"</span></p>
+                <p className="is-italic"> - <span className="has-text-weight-bold">Brett Carpenter</span>, Marketing Ops | Automation | MarTech Seamster</p>
             </div>
         </div>
     );
